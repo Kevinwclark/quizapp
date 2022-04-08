d2f26a6efd42cd445a6b40efd44d2d95e9fccbe1
Kevin Clark <kevinwilliamclark@gmail.com>
2022-04-08 14:48:27 -0500

    Adds alerts, and moves to next question by 'next' button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Question from './components/Question';
-import { Grid } from '@mui/material';
+import { Grid, Alert, AlertTitle, Button,  } from '@mui/material';
 
 
 function App() {
@@ -10,8 +10,7 @@ function App() {
   const [answer, setAnswer] = useState(null);
   const [score, setScore] = useState(0);
   const [questionNumber, setQuestionNumber] = useState(0);
-  console.log(questions.length, questionNumber)
-  console.log('here')
+  console.log(questions[questionNumber] && questions[questionNumber].correct_answer)
 
   const fetchQuestions = async () => {
     const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
@@ -22,33 +21,71 @@ function App() {
     fetchQuestions();
   },[])
 
-  useEffect(() => {
+
+  const nextQuestion = () => {
     if(answer === true) {
       setScore(score + 1);
       setAnswer(null)
-      setQuestionNumber(questionNumber + 1);
+      
     } else if (answer === false) {
       setAnswer(null)
-      setQuestionNumber(questionNumber + 1);
     }
-  },[answer])
+    setQuestionNumber(questionNumber + 1)
+  }
+
+  const resetGame = () => {
+    setScore(0);
+    setQuestionNumber(0);
+    fetchQuestions();
+    setAnswer(null)
+  }
 
   return (
     <div className="App-header">
       <Grid container spacing={4} direction='row' justifyContent='space-evenly'>
 
-
-          <Grid item xs={6} md={8}>
+          <Grid item xs={4} style={{paddingLeft: 50}}>
             <h1>Take the Quiz</h1>
           </Grid>
 
-          <Grid item xs={6} md={4}>
+          <Grid item xs={4} >
             <h1>Score: {score}</h1>
           </Grid>
+          <Grid item xs={4} >
+            <h1>Question: {questionNumber + 1}/10</h1>
+          </Grid>
+          <Grid item xs={4} >
+          {answer === true && (
+            <Alert severity="success">
+            <AlertTitle>Correct</AlertTitle>
+            The correct answer is— <strong>{questions[questionNumber].correct_answer}</strong>
+          </Alert>
+          )}
+          {answer === false && (
+            <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            The correct answer is— <strong>{questions[questionNumber].correct_answer}</strong>
+          </Alert>
+          )}
 
+          </Grid>
         <Grid xs={11}>
           {questions[questionNumber] && <Question key={questions[questionNumber]} setAnswer={setAnswer} {...questions[questionNumber]} />}
         </Grid> 
+        <Grid item xs={3} >
+              <Button 
+                variant="contained"
+                onClick={nextQuestion}
+                size="large">Next
+              </Button>
+          </Grid>
+          <Grid item xs={3} >
+              <Button 
+                variant="contained"
+                onClick={resetGame}
+                size="large">New Game
+              </Button>
+          </Grid>
       </Grid>
     </div>
   );
